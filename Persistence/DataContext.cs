ba3b95900b6b09eab08fbe9eb40e4f248c11702d
BenCook28 <benthomascook28@gmail.com>
2022-09-06 14:48:37 -0400

    before migration

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,7 +1,13 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
 namespace Persistence
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
     }
 }
